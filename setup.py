27c1638df2704441318949a8426f41a503288ce9
Matthew McRaven <mkm302@georgetown.edu>
2020-10-27 15:26:38 -0700

    Add "hello world" example to library.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='nasrl',
+      version='0.1.0',
+      description='Library code for our NAS-RL project.',
+      author='Matthew McRaven',
+      author_email='mkm302@georgetown.edu',
+      packages=['nasrl'],
+     )
