2e88bf6de3eaaff683b494dd9e04d3c7c58c5d3e
Matthew McRaven <mkm302@georgetown.edu>
2020-11-30 15:52:08 -0800

    Move all source code to a `src` directory.
    References:
    `https://blog.ionelmc.ro/2014/05/25/python-packaging/#the-structure`
    `https://docs.pytest.org/en/stable/goodpractices.html#goodpractices`
    
    Having a nested `src` directory prevents all kinds of brokenness.

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import find_packages
 install_reqs = []
 install_reqs.extend(["networkx ~= 2.5", "torch ~= 1.6.0", "torchvision ~= 0.7.0", "librl == 0.2.0"])
 setup(name='nasrl',
-      version='0.2.1',
+      version='0.2.2',
       description='Library code for our NAS-RL project.',
       author='Matthew McRaven',
       author_email='mkm302@georgetown.edu',
       install_reqs=install_reqs,
       python_requires='~= 3.8',
-      packages=find_packages(),
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
      )
